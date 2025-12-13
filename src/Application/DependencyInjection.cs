80fe5ca54ad0814abcd55e3cf4e111707a1035d2
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-12-13 12:57:07 +0100

    update on dependency injection

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using AutoMapper;
 using FluentValidation;
 using MediatR;
+using ProductOrderAPI.Application.Common.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ProductOrderAPI.Application;
@@ -10,7 +11,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
