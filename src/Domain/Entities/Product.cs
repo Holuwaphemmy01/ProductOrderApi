80ba90eeaefdf750365f6e387713cd6da7670639
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-12-14 13:52:31 +0100

    product entity

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -4,10 +4,13 @@ namespace ProductOrderAPI.Domain.Entities;
 
 public class Product : BaseEntity
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public Money Price { get; set; }
-    public int StockQuantity { get; set; }
+    public string? Name { get; set; }
+            public string? Description { get; set; }
+            public Money? Price { get; set; }
+    public int? StockQuantity { get; set; }
+
+    // Private constructor for EF Core
+    private Product() { }
 
     public Product(string name, string description, Money price, int stockQuantity)
     {
@@ -15,5 +18,14 @@ public class Product : BaseEntity
         Description = description;
         Price = price;
         StockQuantity = stockQuantity;
+    }
+
+    public void Update(string name, string description, Money price, int stockQuantity)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+        StockQuantity = stockQuantity;
+        LastModifiedAt = DateTime.UtcNow;
     }
 }
