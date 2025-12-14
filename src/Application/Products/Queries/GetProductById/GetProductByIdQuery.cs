cf157379291126713ba977212608d14a77e234a4
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-12-14 13:48:50 +0100

    get product by id query

--- src/Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
+++ src/Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using ProductOrderAPI.Application.Products.DTOs;
 
-namespace ProductOrderAPI.Application.Products.Queries.GetProductById;
-
-public record GetProductByIdQuery(Guid Id) : IRequest<ProductDto>;
+namespace ProductOrderAPI.Application.Products.Queries.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IRequest<ProductDto>;
+}
